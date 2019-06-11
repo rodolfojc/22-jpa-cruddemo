52abea9547dafcb0b16a2ef1bdf2dbd089903ecb
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-06-11 20:24:25 +0100

    Added code for implementations
    Demo

--- src/main/java/com/springboot/crudemo/dao/EmployeeDAOJpaImpl.java
+++ src/main/java/com/springboot/crudemo/dao/EmployeeDAOJpaImpl.java
@@ -36,18 +36,31 @@ public class EmployeeDAOJpaImpl implements EmployeeDAO{
 	@Override
 	public Employee findById(int theId) {
 		
-		return null;
+		//GET EMPLOYEE
+		Employee theEmployee = this.entityManager.find(Employee.class, theId);
+		
+		//RETURN THE EMPLOYEE
+		return theEmployee;
 	}
 
 	@Override
 	public void addEmployee(Employee theEmployee) {
 		
+		//SAVE OR UPDATE THE EMPLOYEE
+		Employee dbEmployee = this.entityManager.merge(theEmployee);
+		
+		//UPDATE WITH ID
+		theEmployee.setId(dbEmployee.getId());
 		
 	}
 
 	@Override
 	public void deleteEmployeeById(int theId) {
 		
+		//DELETE OBJECT WITH PRIMARY KEY
+		Query theQuery = this.entityManager.createQuery("delete from Employee where id=:employeeId");
+		theQuery.setParameter("employeeId", theId);
+		theQuery.executeUpdate();
 		
 	}
 
